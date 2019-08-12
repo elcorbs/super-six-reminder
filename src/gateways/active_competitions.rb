7182693a85d08a8a25aaf7501397dfd026bdede5
elcorbs <emma@madetech.com>
2019-08-12 20:47:53 +0100

    Move to python

--- src/gateways/active_competitions.rb
+++
@@ -1,8 +0,0 @@
-require 'httparty'
-
-def current_active_competitions
-  response = HTTParty.get('https://super6.skysports.com/api/v2/round/active')
-  if (reponse.code == 200)
-    { start_time: response.body.startDateTime, end_time: response.body.endDateTime }
-  end
-end
--- src/gateways/send_reminder.rb
+++
@@ -1,6 +0,0 @@
-require 'httpary'
-
-def send_reminder
-  HTTParty.post('https://hooks.slack.com/services/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',
-  body: { text: 'Go do your super six: https://super6.skysports.com/' }, headers: {'Content-type': 'application/json'})
-end
