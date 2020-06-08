3d7bb22d909e11ab0ac818561ad284ae539abd70
DanielSuttonRI <dsutton@rti.local>
2020-06-08 11:56:35 +0100

    Env variables and line breaks

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ from datetime import datetime
 from datetime import timedelta
 import random
 import os
+from dotenv import load_dotenv
+
+
+project_folder = os.getcwd()
+load_dotenv(os.path.join(project_folder, '.env'))
 
 getCurrentActiveDates = "https://super6.skysports.com/api/v2/round/active"
 postToSlackURL = os.environ["SLACK_WEB_HOOK"]
 
 def getMatches(data):
   matches = [[x['match']['homeTeam']['name'], x['match']['awayTeam']['name']]  for x in data['scoreChallenges']]
-  return '\\n' + str('\\n'.join([x for x in [' - '.join(match) for match in matches]]))
+  return ' \n' + str('\n'.join([x for x in [' - '.join(match) for match in matches]]))
 
 def generateGenericMessage(startDate, endDate, today, data):
-  messages = ["Do you super six you silly sausage.", "Do you want to win a million or not? Do your super six.", "Be super slick and do your super six.", "Come on, come on get your super six on, it's {} morning and it won't take long".format(str(today.strftime("%A")))]
+  messages = ["Do your super six you silly sausage.", "Do you want to win a million or not? Do your super six.", "Be super slick and do your super six.", "Come on, come on get your super six on, it's {} morning and it won't take long".format(str(today.strftime("%A")))]
   messageNo = random.randint(0, len(messages) - 1)
   return "{message} You have {days} days left! - https://super6.skysports.com/".format(message = messages[messageNo], days=str((endDate - today).days))
 
