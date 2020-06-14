12d6a9c6bb6dd6df70300aa1a88a761bf54a9f08
elcorbs <emma@madetech.com>
2020-06-14 20:37:30 +0100

    Compile match data messages

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 import random
 import os
 from dotenv import load_dotenv
-from scores import get_matches
+from match_data import MatchData
 
 
 project_folder = os.getcwd()
@@ -13,28 +13,27 @@ load_dotenv(os.path.join(project_folder, '.env'))
 
 getCurrentActiveDates = "https://super6.skysports.com/api/v2/round/active"
 postToSlackURL = os.environ["SLACK_WEB_HOOK"]
-
-def getMatches(data):
-  matches = [[x['match']['homeTeam']['name'], x['match']['awayTeam']['name']]  for x in data['scoreChallenges']]
-  return ' \n' + str('\n'.join([x for x in [' - '.join(match) for match in matches]]))
+match_data = MatchData()
 
 def generateGenericMessage(startDate, endDate, today, data):
   messages = ["Do your super six you silly sausage.", "Do you want to win a million or not? Do your super six.", "Be super slick and do your super six.", "Come on, come on get your super six on, it's {} morning and it won't take long".format(str(today.strftime("%A")))]
   messageNo = random.randint(0, len(messages) - 1)
   return "{message} You have {days} days left! - https://super6.skysports.com/".format(message = messages[messageNo], days=str((endDate - today).days))
 
+def parse_date(timestamp):
+  return datetime.strptime(timestamp[0:10], "%Y-%m-%d").date()
+
 request = requests.get(getCurrentActiveDates)
 data = request.json()
 
 today = datetime.today().date()
-startDate = datetime.strptime(data['startDateTime'][0:10], "%Y-%m-%d").date()
-
-endDate = datetime.strptime(data['endDateTime'][0:10], "%Y-%m-%d").date()
+startDate = parse_date(data['startDateTime'])
+endDate = parse_date(data['endDateTime'])
 
 message = generateGenericMessage(startDate, endDate, today, data)
 
 if startDate + timedelta(days=1) == today:
-  message += get_matches(data) 
+  message += match_data.retrieve(data) 
 
 if ((today >= startDate) & (today <= endDate)):
   r = requests.post(
