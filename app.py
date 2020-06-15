36e279e86070f904a60d3a97950527b3e4ea9304
elcorbs <emma@madetech.com>
2020-06-15 17:44:29 +0100

    Indent response

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ user_model = UserModel()
 def slack_proxy_response():
     if request.mimetype == 'application/json':
       data = request.get_json()
-      print(data)
       if data["actions"][0]["value"] == "remove_me":
         user_model.entered_this_round(data["user"]["id"])
-    return Response(status=200)
+        return Response(status=200)
+        
 if __name__ == "__main__":    
     app.run()
