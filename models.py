4ce57e95eb641334e2f7b06d993f8f76e84b91fa
elcorbs <emma@madetech.com>
2020-06-15 18:17:57 +0100

    Use cursor instead of connection

--- models.py
+++ models.py
@@ -6,12 +6,12 @@ class UserModel:
     def __init__(self):
         DATABASE_URL = os.environ['DATABASE_URL']
         self.conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    
+        self.cursor = self.conn.cursor()    
     def entered_this_round(self, userId):
         get_user_query = """
         SELECT * FROM Users WHERE UserID = {userId};
         """
-        user = self.conn.execute(get_user_query)
+        user = self.cursor.execute(get_user_query)
         if (user.rowcount == 0):
           query = """
           INSERT INTO Users (UserId, EnteredThisRound) VALUES ({userId}, TRUE);
@@ -20,17 +20,17 @@ class UserModel:
           query = """
           UPDATE Users SET EnteredThisRound = TRUE WHERE UserId = {userId};
           """  
-        self.conn.execute(query)
+        self.cursor.execute(query)
 
     def users_still_outstanding(self):
         query = """
         SELECT COUNT(*) FROM Users WHERE EnteredThisRound = FALSE;
         """
-        outstanding = self.conn.execute(query)
+        outstanding = self.cursor.execute(query)
         return outstanding > 0 
 
     def start_new_round(self):
         query = """
         UPDATE Users SET EnteredThisRound = FALSE;
         """
-        self.conn.execute(query)
+        self.cursor.execute(query)
