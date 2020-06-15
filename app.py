292f87aa8fe19551cced933d5908390ed018ae07
elcorbs <emma@madetech.com>
2020-06-15 18:03:51 +0100

    parse data

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify, Response
 from models import UserModel
+import json
 app = Flask(__name__)             
 
 user_model = UserModel()
 @app.route("/slack-response", methods=['POST'])
 def slack_proxy_response():
-    data = request.form
-    print(request.form)
+    data = json.loads(request.form[0][1])
+    print(data)
     user_model.entered_this_round(data["user"]["id"])
     return Response(status=200)
 if __name__ == "__main__":    
