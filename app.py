1ffe4be603bebc5b43ab926fc0b2329931eed3d7
elcorbs <emma@madetech.com>
2020-06-15 17:04:05 +0100

    Access as key value pair'

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 user_model = UserModel()
 @app.route("/slack-response", methods=['POST'])                  
 def slack_proxy_response():                    
-    if request.data.actions[0].value == "remove_me":
+    print(request.data)
+    if request.data["actions"][0].value == "remove_me":
       user_model.entered_this_round(request.data.user.id)
     return Response(status=200)
 if __name__ == "__main__":    
