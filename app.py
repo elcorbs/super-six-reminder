dacbf05858d485041c6a6850be1e012e2e146a42
elcorbs <emma@madetech.com>
2020-06-15 16:07:23 +0100

    Return success reponse

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from models import Schema, UserModel
 app = Flask(__name__)             
 
@@ -8,7 +8,7 @@ def slack_proxy_response():
     print(request)
     if request.data.actions[0].value == "remove_me":
       user_model.entered_this_round(request.data.user.id)
-    return "Hello World!"      
+    return Response(status=200)
 if __name__ == "__main__":    
     Schema()
     app.run()
