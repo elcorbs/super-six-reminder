321b25ddf6d2c4e002335240afae6d3887e4b986
elcorbs <emma@madetech.com>
2020-06-15 18:25:51 +0100

    Db connections

--- models.py
+++ models.py
@@ -9,28 +9,30 @@ class UserModel:
         self.cursor = self.conn.cursor()    
     def entered_this_round(self, userId):
         get_user_query = """
-        SELECT * FROM Users WHERE UserID = {userId};
+        SELECT * FROM Users WHERE UserID = %s;
         """
-        user = self.cursor.execute(get_user_query)
-        if (user.rowcount == 0):
+        self.cursor.execute(get_user_query, userId)
+        if (self.cursor.rowcount == 0):
           query = """
-          INSERT INTO Users (UserId, EnteredThisRound) VALUES ({userId}, TRUE);
+          INSERT INTO Users (UserId, EnteredThisRound) VALUES (%s, TRUE);
           """
         else:
           query = """
-          UPDATE Users SET EnteredThisRound = TRUE WHERE UserId = {userId};
+          UPDATE Users SET EnteredThisRound = TRUE WHERE UserId = %s;
           """  
-        self.cursor.execute(query)
+        self.cursor.execute(query, userId)
+        self.conn.commit()
 
     def users_still_outstanding(self):
         query = """
-        SELECT COUNT(*) FROM Users WHERE EnteredThisRound = FALSE;
+        SELECT * FROM Users WHERE EnteredThisRound = FALSE;
         """
         outstanding = self.cursor.execute(query)
-        return outstanding > 0 
+        return outstanding.rowcount > 0 
 
     def start_new_round(self):
         query = """
         UPDATE Users SET EnteredThisRound = FALSE;
         """
         self.cursor.execute(query)
+        self.conn.commit()
