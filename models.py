2391f2dced36e6130f917460006d9f609e72b35f
elcorbs <emma@madetech.com>
2020-06-15 17:00:53 +0100

    add user to db if they dont exist

--- models.py
+++ models.py
@@ -23,22 +23,28 @@ class UserModel:
     def __init__(self):
         self.conn = sqlite3.connect('users.db')
     
-    def register_user(self, userId):
-        query = """
-        INSERT INTO Users (UserId, ReceiveAlerts VALUE ({userId}, 1);
-        """
-        self.conn.execute(query)
     def entered_this_round(self, userId):
-        query = """
-        UPDATE Users SET EnteredThisRound = 1 WHERE UserId = {userId};
-        """  
+        get_user_query = """
+        SELECT * FROM Users WHERE UserID = {userId};
+        """
+        user = self.conn.execute(get_user_query)
+        if (user.rowcount == 0):
+          query = """
+          INSERT INTO Users (UserId, EnteredThisRound) VALUES ({userId}, 1);
+          """
+        else:
+          query = """
+          UPDATE Users SET EnteredThisRound = 1 WHERE UserId = {userId};
+          """  
         self.conn.execute(query)
+
     def users_still_outstanding(self):
         query = """
         SELECT COUNT(*) FROM Users WHERE EnteredThisRound = 0;
         """
         outstanding = self.conn.execute(query)
         return outstanding != 2
+
     def start_new_round(self):
         query = """
         UPDATE Users SET EnteredThisRound = 0;
