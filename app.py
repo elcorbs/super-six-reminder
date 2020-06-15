9c53086db699ec2c2cea1195e0ff9532dfe03b2a
elcorbs <emma@madetech.com>
2020-06-15 17:23:26 +0100

    parse json

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from models import Schema, UserModel
 app = Flask(__name__)             
 
 user_model = UserModel()
-@app.route("/slack-response", methods=['POST'])                  
+@app.route("/slack-response", methods=['POST'])
 def slack_proxy_response():
-    request_json = request.get_json()
-    print(request_json)
-    if request_json["actions"][0]["value"] == "remove_me":
-      user_model.entered_this_round(request_json["user"]["id"])
+    if request.mimetype == 'application/json':
+      data = request.get_json()
+      print(data)
+      if data["actions"][0]["value"] == "remove_me":
+        user_model.entered_this_round(data["user"]["id"])
     return Response(status=200)
 if __name__ == "__main__":    
     Schema()
