c7c247041ccc125e6ae49c3f8bca43f609cdce11
elcorbs <emma@madetech.com>
2020-06-15 17:59:12 +0100

    print dictionary

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ user_model = UserModel()
 @app.route("/slack-response", methods=['POST'])
 def slack_proxy_response():
     data = request.form
-    print(request.form.keys())
+    print(request.form)
     user_model.entered_this_round(data["user"]["id"])
     return Response(status=200)
 if __name__ == "__main__":    
