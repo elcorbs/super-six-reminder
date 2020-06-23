1143154b89bf9296b1a3b0357da206ede7696520
DanielSuttonRI <dsutton@rti.local>
2020-06-23 10:31:38 +0100

    Correct return statement and remove redundent impo

--- models.py
+++ models.py
@@ -1,38 +1,39 @@
-import sqlite3 
 import os
 import psycopg2
 
+
 class UserModel:
     def __init__(self):
-        DATABASE_URL = os.environ['DATABASE_URL']
-        self.conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-        self.cursor = self.conn.cursor()    
+        DATABASE_URL = os.environ["DATABASE_URL"]
+        self.conn = psycopg2.connect(DATABASE_URL, sslmode="require")
+        self.cursor = self.conn.cursor()
+
     def entered_this_round(self, userId):
         get_user_query = """
         SELECT * FROM users WHERE UserID = %s;
         """
-    
+
         self.cursor.execute(get_user_query, (userId,))
         row_count = self.cursor.rowcount
-        if (row_count == 0):
-          query = """
+        if row_count == 0:
+            query = """
           INSERT INTO users (UserId, EnteredThisRound) VALUES (%s, TRUE);
           """
         else:
-          query = """
+            query = """
           UPDATE users SET EnteredThisRound = TRUE WHERE UserId = %s;
-          """  
+          """
         self.cursor.execute(query, (userId,))
         self.commit_and_close()
 
     def users_still_outstanding(self):
-        outstanding = self.get_row_count("""
+        outstanding = self.get_row_count(
+            """
         SELECT * FROM users WHERE EnteredThisRound = FALSE;
-        """)
-        total = self.get_row_count("""
-        SELECT * FROM users;
-        """)
-        return total < 2 | outstanding > 0 
+        """
+        )
+
+        return outstanding > 0
 
     def start_new_round(self):
         query = """
@@ -40,7 +41,7 @@ class UserModel:
         """
         self.cursor.execute(query)
         self.commit_and_close()
-    
+
     def get_row_count(self, query):
         self.cursor.execute(query)
         total = self.cursor.rowcount
